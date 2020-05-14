5f978a16f30673f8cf1270735557d7025888bb89
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-14 09:36:39 +0800

    Added insertOne() method to config/orm.js

--- config/orm.js
+++ config/orm.js
@@ -2,8 +2,16 @@ const connection = require("./connection");
 
 let orm = {
     selectAll: function (tableInput) {
-        var queryString = "SELECT * FROM ??";
+        var queryString = `SELECT * FROM ??`;
         connection.query(queryString, [tableInput], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+
+    insertOne: function (tableInput, insertColumn1, insertColumn2, insertName, insertStatus) {
+        var queryString = `INSERT INTO ??  (??, ??) VALUES (?, ?)`;
+        connection.query(queryString, [tableInput, insertColumn1, insertColumn2, insertName, insertStatus], function (err, result) {
             if (err) throw err;
             console.log(result);
         });
