6f444fa2aa1c95aafeb59793ed4ca6d47c45c1b1
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-14 09:28:32 +0800

    Added selectAll() method to config/orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,13 @@
 const connection = require("./connection");
+
 let orm = {
     selectAll: function (tableInput) {
-        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+        var queryString = "SELECT * FROM ??";
         connection.query(queryString, [tableInput], function (err, result) {
             if (err) throw err;
             console.log(result);
         });
     }
 };
+
 module.exports = orm;
