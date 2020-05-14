72cab48a80af4bf89cf1ffd69009a30b4e34bc56
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-14 09:24:15 +0800

    Created /db folder containing schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('steak', 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken', 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('vegetarian', 1);
