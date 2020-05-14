3cd4d92a558275738b062cdaa4f08540d30694fd
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-14 09:43:50 +0800

    Added updateOne() method to config/orm.js

--- config/orm.js
+++ config/orm.js
@@ -9,9 +9,17 @@ let orm = {
         });
     },
 
-    insertOne: function (tableInput, insertColumn1, insertColumn2, insertName, insertStatus) {
+    insertOne: function (tableInput, insertColumnOne, insertColumnTwo, insertName, insertStatus) {
         var queryString = `INSERT INTO ??  (??, ??) VALUES (?, ?)`;
-        connection.query(queryString, [tableInput, insertColumn1, insertColumn2, insertName, insertStatus], function (err, result) {
+        connection.query(queryString, [tableInput, insertColumnOne, insertColumnTwo, insertName, insertStatus], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+
+    updateOne: function (tableInput, setColumnOne, setValue, columnOne, valueOne) {
+        var queryString = `UPDATE ?? SET ?? = ? WHERE ?? = ?`;
+        connection.query(queryString, [tableInput, setColumnOne, setValue, columnOne, valueOne], function (err, result) {
             if (err) throw err;
             console.log(result);
         });
