eeef2c35334c7bd75e85c6f2b7591e86da5842e1
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-15 11:39:31 +0800

    Added POST and PUT ajax calls

--- server.js
+++ server.js
@@ -1,10 +1,52 @@
 const express = require("express");
 const routes = require("./controllers/burgers_controller.js")
 const PORT = process.env.PORT || 8080;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+$(function () {
+
+    $(".devourBurger").on("click", function(event) {
+        let id = $(this).data("id");
+    
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT"
+        }).then(
+          function() {
+            console.log("devoured burger id: ", id);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        let newBurger = {
+            burger_name: $("#burgerId").val().trim(),
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("Burger added successfuly");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
 
 // Start the server
 app.listen(PORT, function () {
