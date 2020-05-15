b14ea37236c6d0c63f9476d0a7a8a5603e2d8c65
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-15 10:48:46 +0800

    Removed server initialisation from controllers/burgers_contoller.js and added it to server.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,43 +1,39 @@
 const express = require("express");
-const model = require("../models/burger.js");
+const burgerQuery = require("../models/burger.js");
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+let routes = {
+    getBurgers:
+        app.get("/", function (req, res) {
+            burgerQuery.all
+            if (err) {
+                return res.status(500).end();
+            }
+            if (result.length === 0) {
+                return res.status(404).end();
+            }
+            res.render(result);
+            res.status(200);
+        }),
 
-app.get("/", function (req, res) {
-    model.all
-    if (err) {
-        return res.status(500).end();
-    }
-    if (result.length === 0) {
-        return res.status(404).end();
-    }
-    res.render(result);
-    res.status(200);
-});
+    // Update burger status
+    devourBurger:
+        app.put("/api/burger/:id", function (req, res) {
+            burgerQuery.update
+            if (err) {
+                return res.status(500).end();
+            }
+            res.status(200);
+        }),
 
-// Update burger status
-app.put("/api/burger/:id", function (req, res) {
-    model.update
-    if (err) {
-        return res.status(500).end();
-    }
-    res.status(200);
-});
-
-// Create a new burger
-app.post("/api/burger", function (req, res) {
-    model.insert;
-    if (err) {
-        return res.status(500).end();
-    }
-    res.status(200);
-});
-
-// Start the server
-app.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:" + PORT);
-});
+    // Create a new burger
+    newBurger:
+        app.post("/api/burger", function (req, res) {
+            burgerQuery.insert;
+            if (err) {
+                return res.status(500).end();
+            }
+            res.status(200);
+        })
+}
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const routes = require("./controllers/burgers_controller.js")
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
