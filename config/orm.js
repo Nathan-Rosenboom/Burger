5a36c7b52233e5460e5feb968942e5d6a95def61
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-20 13:52:43 +0800

    Fixed double quotes in queries

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,5 @@
 const connection = require("../config/connection.js");
 
-function printQuestionMarks(num) {
-    let arr = [];
-    for (let i = 0; i < num; i++) {
-        arr.push("?");
-    }
-    return arr.toString();
-}
 function objToSql(ob) {
     let arr = [];
     for (let key in ob) {
@@ -32,7 +25,7 @@ let orm = {
         });
     },
     create: function (table, colOne, valOne, cb) {
-        let queryString = "INSERT INTO " + table + " (" + colOne + ") VALUES (" + valOne + ");";
+        let queryString = "INSERT INTO " + table + " (" + colOne + ") VALUES ('" + valOne + "');";
 
         console.log(queryString);
 
