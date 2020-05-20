694a21c4c437dbb5648cecd7037c00b88796dab6
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-20 14:28:07 +0800

    Added connection to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
 // mysql connection parameters
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "RufusDog2019!",
-    database: "burgers_db"
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "RufusDog2019!",
+        database: "burgers_db"
+    });
+};
 // Initialise the connection to burgers_db
 connection.connect(function (err) {
     if (err) {
